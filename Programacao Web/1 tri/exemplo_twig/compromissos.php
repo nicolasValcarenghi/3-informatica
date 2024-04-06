f11c3dddbe83d83e40ada43d9cba5f6285c211b4
nicolasValcarenghi <nicki.valca10@gmail.com>
2024-04-06 13:52:21 -0300

    Terminando o arquivo

--- Programacao Web/1 tri/exemplo_twig/compromissos.php
+++ Programacao Web/1 tri/exemplo_twig/compromissos.php
@@ -1,9 +1,24 @@
 <?php
+use Carbon\Carbon;
 require('twig_carregar.php');
 require('inc/banco.php');
 
 $compromissos = $pdo->query('SELECT * FROM compromissos');
 
-echo $twig->render('compromissos.html', ["compromissos" => $compromissos]);
 
 $compromissos = $compromissos->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($compromissos as $index => $compromisso) {
+    $dataFormatada = Carbon::parse($compromisso['data']);
+    if($dataFormatada -> isWeekend()) {
+        $compromisso['fimDeSemana'] = 'Sim';
+        $compromissos[$index] = $compromisso;
+    }
+    else{
+        $compromisso['fimDeSemana'] = 'Não';
+        $compromissos[$index] = $compromisso;
+    }
+}
+
+
+echo $twig->render('compromissos.html', ["compromissos" => $compromissos]);
